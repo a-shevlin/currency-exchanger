ce68a4d88f17d76731c7313c21b3a87207b5272f
Alex Shevlin <alexshevlin1@gmail.com>
2022-07-01 09:52:51 -0700

    add getElements to return response of ExchangeService

--- src/index.js
+++ src/index.js
@@ -2,4 +2,22 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Template from './js/template.js';
+import ExchangeService from './js/currency-exchanger-api';
+
+// function clearFields() {
+
+// }
+
+function getElements(response) {
+  let newAmount =  response.conversion_result;
+  console.log(newAmount);
+}
+
+$(document).ready(function() {
+  let amount = 1;
+  let currency = 'SEK';
+  ExchangeService.getCurrency(currency, amount)
+    .then(function(response) {
+      getElements(response);
+    });
+});
