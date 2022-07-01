bb3f8e13ac943cbe5af2bca9e000ba94fc00e0ec
Alex Shevlin <alexshevlin1@gmail.com>
2022-07-01 10:06:49 -0700

    update to include currencyFrom and currencyTo variables

--- src/js/currency-exchanger-api.js
+++ src/js/currency-exchanger-api.js
@@ -1,8 +1,8 @@
 //business logic
 
 export default class ExchangeService {
-  static getCurrency(currency, amount) {
-    let url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency}/${amount}`;
+  static getCurrency(currencyFrom, currencyTo, amount) {
+    let url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currencyFrom}/${currencyTo}/${amount}`;
     return fetch(url)
       .then(function(response) {
         if(!response.ok) {
